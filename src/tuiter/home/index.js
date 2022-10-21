b59be0381ee4dd16ffd77f6e6d0105ae3c56c2fd
Chuying Huang <daisychuyinghuang@gmail.com>
2022-10-21 11:54:43 -0700

    Completed A6 Assignment

---
+++ src/tuiter/home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PostList from "../post-list";
+
+
+const HomeComponent = () => {
+    return (
+        <div>
+            <div className="">
+                <PostList/>
+            </div>
+        </div>
+        );
+}
+
+export default HomeComponent;
--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,10 +1,30 @@
-import Nav from "../nav";
+import ExploreComponent from "./explore";
+import HomeComponent from "./home";
+import NavigationSidebar from "./navigation-sidebar";
+import WhoToFollowList from "./who-to-follow-list";
+import {Routes, Route} from "react-router";
+
+
+
+
 function Tuiter() {
-    return(
-        <div>
-            <Nav/>
-            <h1>Tuiter</h1>
+    return (
+        <div className="row mt-2">
+            <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+                <NavigationSidebar active="explore"/>
+            </div>
+            <div className="col-9 col-md-10 col-lg-7 col-xl-6"
+                 style={{"position": "relative"}}>
+                <Routes>
+                    <Route path="/home"    element={<HomeComponent/>}/>
+                    <Route path="/explore" element={<ExploreComponent/>}/>
+                </Routes>
+            </div>
+            <div className="col-3 col-lg-2 col-xl-4">
+                <WhoToFollowList/>
+            </div>
         </div>
     );
 }
+
 export default Tuiter
